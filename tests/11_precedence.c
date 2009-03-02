5c5d33ee34b393eada94be5bd2379a3687960722
zik.saleeba <zik.saleeba@21eae674-98b7-11dd-bd71-f92a316d2d60>
2009-03-02 06:46:03 +0000

    Added new operator precedence tests
    git-svn-id: http://picoc.googlecode.com/svn/trunk@140 21eae674-98b7-11dd-bd71-f92a316d2d60

---
+++ tests/11_precedence.c
@@ -0,0 +1,33 @@
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int x;
+int y;
+
+a = 12;
+b = 34;
+c = 56;
+d = 78;
+e = 0;
+f = 1;
+
+printf("%d\n", (a + b, c + d));
+printf("%d\n", (x = a + b, y = c + d));
+printf("%d\n", a ? b+c : c+d);
+printf("%d\n", a ? b+c : c+d);
+printf("%d\n", a || b ? b+c : c+d);
+printf("%d\n", e || e && f);
+printf("%d\n", e || f && f);
+printf("%d\n", e && e || f);
+printf("%d\n", e && f || f);
+printf("%d\n", a && f | f);
+printf("%d\n", a | b ^ c & d);
+printf("%d, %d\n", a == a, a == b);
+printf("%d, %d\n", a != a, a != b);
+printf("%d\n", a != b && c != d);
+printf("%d\n", a + b * c / f);
+printf("%d\n", a + b * c / f);
+
