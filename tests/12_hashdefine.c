2e0ff8774c4b19c54a6641108ab9aef0581be4e2
zik.saleeba <zik.saleeba@21eae674-98b7-11dd-bd71-f92a316d2d60>
2009-03-08 01:10:29 +0000

    Added missing tests
    git-svn-id: http://picoc.googlecode.com/svn/trunk@166 21eae674-98b7-11dd-bd71-f92a316d2d60

---
+++ tests/12_hashdefine.c
@@ -0,0 +1,6 @@
+#define FRED 12
+#define BLOGGS(x) (12*(x))
+
+printf("%d\n", FRED);
+//printf("%d, %d, %d\n", BLOGGS(1), BLOGGS(2), BLOGGS(3));
+
---
+++ tests/13_integer_literals.c
@@ -0,0 +1,12 @@
+int a = 24680;
+int b = 01234567;
+int c = 0x2468ac;
+int d = 0x2468AC;
+int e = 0b010101010101;
+
+printf("%d\n", a);
+printf("%d\n", b);
+printf("%d\n", c);
+printf("%d\n", d);
+printf("%d\n", e);
+
