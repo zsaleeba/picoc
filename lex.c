da2cd4805d6a43d39489dc3c4a3ec172d2727d6a
zik.saleeba <zik.saleeba@21eae674-98b7-11dd-bd71-f92a316d2d60>
2009-05-27 03:23:41 +0000

    Fixed an annoying bug which looked like a floating point issue but was in fact a string lexer issue.
    git-svn-id: http://picoc.googlecode.com/svn/trunk@291 21eae674-98b7-11dd-bd71-f92a316d2d60

--- lex.c
+++ lex.c
@@ -591,7 +591,7 @@ enum LexToken LexGetToken(struct ParseState *Parser, struct Value **Value, int I
         { 
             switch (Token)
             {
-                case TokenStringConstant:       LexValue.Typ = CharPtrType; break;
+                case TokenStringConstant:       LexValue.Typ = CharPtrType; LexValue.Val->Pointer.Offset = 0; break;
                 case TokenIdentifier:           LexValue.Typ = NULL; break;
                 case TokenIntegerConstant:      LexValue.Typ = &IntType; break;
                 case TokenCharacterConstant:    LexValue.Typ = &CharType; break;
