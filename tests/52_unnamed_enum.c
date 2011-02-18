72b7c7de0b5d8aaa4907295cc01ae7af2fea3686
zik.saleeba <zik.saleeba@21eae674-98b7-11dd-bd71-f92a316d2d60>
2011-02-18 03:33:58 +0000

    Added test for unnamed enums
    git-svn-id: http://picoc.googlecode.com/svn/trunk@554 21eae674-98b7-11dd-bd71-f92a316d2d60

---
+++ tests/52_unnamed_enum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+enum fred { a, b, c };
+
+printf("a=%d\n", a);
+printf("b=%d\n", b);
+printf("c=%d\n", c);
+
+enum fred d;
+
+typedef enum { e, f, g } h;
+typedef enum { i, j, k } m;
+
+printf("e=%d\n", e);
+printf("f=%d\n", f);
+printf("g=%d\n", g);
+
+printf("i=%d\n", i);
+printf("j=%d\n", j);
+printf("k=%d\n", k);
+
+void main()
+{
+}
